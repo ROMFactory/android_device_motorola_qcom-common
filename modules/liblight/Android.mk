452610c71f597b8e43083bcbd912b764f4514da8
pcarenza <pcarenza@gmail.com>
2013-10-15 19:49:57 -0400

    make battery led user-switchable
    Change-Id: I5004be7550a0b4cd7216a090d265f1b2d00ffb5e

--- modules/liblight/Android.mk
+++ modules/liblight/Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_USES_MOTOROLA_MSM8960_COMMON_LIBLIGHT),true)
+#ifeq ($(TARGET_USES_MOTOROLA_MSM8960_COMMON_LIBLIGHT),true)
 LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
@@ -26,4 +26,4 @@ LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
-endif
+#endif
